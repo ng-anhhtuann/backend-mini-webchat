f7c8fd071c7997109fd2914aad59bf79ee066216
Nguyen Anh Tuan <nguyenanhtuan1232@gmail.com>
2023-02-19 15:55:54 +0700

    Initial project

---
+++ src/main/java/com/example/miniwebapp/MiniWebappApplication.java
@@ -0,0 +1,13 @@
+package com.example.miniwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniWebappApplication.class, args);
+	}
+
+}
