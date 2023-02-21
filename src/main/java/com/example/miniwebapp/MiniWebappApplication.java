14c73813a945278d55df0c466b255fa6eed056e4
Nguyen Anh Tuan <nguyenanhtuan1232@gmail.com>
2023-02-22 00:32:52 +0700

    Origin

--- src/main/java/com/example/miniwebapp/MiniWebappApplication.java
+++ src/main/java/com/example/miniwebapp/MiniWebappApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MiniWebappApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Worlds");
-		//Hello Worlds
 		SpringApplication.run(MiniWebappApplication.class, args);
 	}
 
