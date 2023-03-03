4acee141997623e3763c36e991288eed82cfd14d
Nguyen Anh Tuan <nguyenanhtuan1232@gmail.com>
2023-03-03 18:06:33 +0700

    Test login API

--- src/test/java/com/example/miniwebapp/UserServiceTest.java
+++ src/test/java/com/example/miniwebapp/UserServiceTest.java
@@ -1,9 +1,10 @@
 package com.example.miniwebapp;
 
 import com.example.miniwebapp.Config.Database;
+import com.example.miniwebapp.Models.Form.LogIn;
 import com.example.miniwebapp.Models.Response;
 import com.example.miniwebapp.Models.Form.SignUp;
-import com.example.miniwebapp.Repository.UserRepository;
+import com.example.miniwebapp.Repository.Implementation.UserRepository;
 import org.junit.jupiter.api.*;
 import org.mockito.InjectMocks;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,19 +22,30 @@ class userRepositoryTest {
 
     @Test
     public void testInsertSignUpUser() {
-        // Create a new user object with valid data
         SignUp signUp = new SignUp("john.doe", "John Doe", "", "johndoe15650@gmail.com");
-
-        // Call the signUpUser method and check the response
         Response response = (Response) userRepository.signUpUser(signUp);
         Assertions.assertEquals("Fill all the cells",response.getMessage());
     }
+
     @Test
     public void testInsertSignUpExistedUser(){
         SignUp signUp = new SignUp("john.doe","asdfasdasd","asdr325sdf23r","johndoe15650@gmail.com");
-        
         Response response = (Response) userRepository.signUpUser(signUp);
         Assertions.assertEquals("Username or Mail existed",response.getMessage());
+    }
+
+    @Test
+    public void testLoginSuccess(){
+        LogIn logIn = new LogIn("chienbinh156","vipproluxury");
+        Response response = (Response) userRepository.signInUser(logIn);
+        Assertions.assertEquals("Login successfully!",response.getMessage());
+    }
+
+    @Test
+    public void testLoginFail(){
+        LogIn logIn = new LogIn("chienbinh156","vipprolasduxury");
+        Response response = (Response) userRepository.signInUser(logIn);
+        Assertions.assertEquals("Wrong password!",response.getMessage());
     }
 }
 
