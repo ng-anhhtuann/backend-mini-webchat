f767e30390d28164768c09e5d27337600b3bb2dd
Nguyen Anh Tuan <nguyenanhtuan1232@gmail.com>
2023-03-03 17:36:20 +0700

    Change place of folder

--- src/main/java/com/example/miniwebapp/Repository/Manager/UserInterface.java
+++ src/main/java/com/example/miniwebapp/Repository/Manager/UserInterface.java
@@ -1,7 +1,11 @@
-package com.example.miniwebapp.Repository;
+package com.example.miniwebapp.Repository.Manager;
 
 import com.example.miniwebapp.Models.Form.LogIn;
 import com.example.miniwebapp.Models.Form.SignUp;
+import com.example.miniwebapp.Models.User;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface UserInterface {
     Object signUpUser(SignUp signUp);
